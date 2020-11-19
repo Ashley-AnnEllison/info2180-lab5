b0479f6637901e0a01d869c55affd3baab0443d1
Ashley-Ann Ellison <aashleyellison51@gmail.com>
2020-11-19 14:55:30 -0500

    Exercise 4 Added

--- world.php
+++ world.php
@@ -20,8 +20,33 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <?php //$query = filter_input(INPUT_GET, "query", FILTER_SANITIZE_STRING); ?>
 
+
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence Year</th>
+    <th>Head of State</th>
+  </tr>";
+
+<?php
+foreach($results as $row):
+  echo "<tr><td>".$row['name']."</td>;
+  echo "<tr><td>".$row['continent']."</td>;
+  echo "<tr><td>".$row['independence_year']."</td>;
+  echo "<tr><td>".$row['head_of_state']."</td>;
+?>
+</table>
+
+
+
+
+
+<?php
+/*
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
-</ul>
+</ul>*/
+?>
